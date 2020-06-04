2b769615fad926ddbb4b6905c8da69fc5fb0d7a3
Ilya <45998835+ilyapishchulin@users.noreply.github.com>
2020-06-05 00:30:15 +0300

    Add example code

---
+++ Navigation and change scheme (Class)/src/App.js
@@ -0,0 +1,80 @@
+import React from 'react'; // React
+import bridge from '@vkontakte/vk-bridge'; // VK Bridge
+import { View, ConfigProvider } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css'; // Импортируем css
+
+// Импортируем панели
+import Home from './panels/home.js'
+import Second from './panels/second.js'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activePanel: 'home', // Начальная панель.
+            history: ['home'],
+        }
+    }
+
+    goToPage( name ) { // В качестве аргумента принимаем id панели для перехода
+        window.history.pushState( { panel: name }, name ); //  Создаём новую запись в истории браузера
+        this.setState({ 
+            activePanel: name, // Меняем активную панель
+            history: [...this.state.history, name] // Добавляем в массив панель, на которую перешли
+        })
+    }
+
+    goBack = () => {
+        var history = this.state.history;
+        
+        if( history.length === 1  ) {  // Если в массиве одно значение:
+          bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
+        } else if ( history.length > 1 ) {  // Если в массиве больше одного значения:
+          history.pop() // Удаляем последний элемент в массиве.
+          this.setState({ activePanel: history[history.length - 1] }) // Изменяем массив с иторией и меняем активную панель.
+        }
+    }
+
+    componentDidMount() {
+        bridge.subscribe(({ detail: { type, data }}) => { // Подписываемся на события bridge.
+			if ( type === 'VKWebAppUpdateConfig' ) { // Получаем тему клиента.
+                this.setState({scheme: data.scheme})
+            }
+        })
+        // Обработчик события изменения истории браузера для работы навигационных кнопок.
+        window.addEventListener('popstate', () => this.goBack());
+    }
+
+    SetScheme( scheme, isChange = false ) {
+        const lights = ['bright_light', 'client_light'];
+        const isLight = lights.includes( scheme );
+        
+        UpdateTheme( isChange ? !isLight : isLight );
+    }
+     
+    UpdateTheme( isLight ) {
+        this.setState({ scheme: isLight ? 'bright_light' : 'space_gray' });
+        bridge.send('VKWebAppSetViewSettings', {
+           'status_bar_style': isLight ? 'dark' : 'light',
+           'action_bar_color': isLight ? '#000' : '#ffff'
+        });
+    }
+    
+    render() {
+        return(
+            <ConfigProvider isWebView={true}>
+                <View 
+                    activePanel={this.state.activePanel}
+                    history={this.state.history}
+                    onSwipeBack={this.goBack}
+                >
+                    <Home id="home" this={this}/>
+                    <Second id="second" this={this}/>
+                </View>
+            </ConfigProvider>
+        );
+    }
+}
+
+export default App;
---
+++ Navigation and change scheme (Class)/src/panels/second.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+
+const second = props => (
+    <Panel id={props.id}>
+        <PanelHeader left={<PanelHeaderBack onClick={() => window.history.back()}/>}>
+            {props.this.state.scheme}
+        </PanelHeader>
+            <img style={{
+                    height: '50%', 
+                    width: '50%', 
+                    marginLeft: '25%'
+            }} alt="" src='https://vk.com/sticker/1-16263-256'/>
+
+            <Div>
+                <Button onClick={() => props.this.UpdateTheme( props.this.state.scheme, true )} size="xl" mode="secondary">Сменить тему на альтернативную</Button>
+            </Div>
+    </Panel>
+);
+
+export default second;
